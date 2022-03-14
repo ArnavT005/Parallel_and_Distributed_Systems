acfc6009eaf530334eb016c65b6fedbc728c031d
Harsh Agrawal <harsh.ag14901@gmail.com>
2022-03-14 15:21:26 +0530

    Serialize and desirialize vectors to binary

--- Assignment-3/main.cpp
+++ Assignment-3/main.cpp
@@ -8,149 +8,155 @@ template <typename T, typename K>
 using P = std::pair<T, K>;
 
 namespace metric {
-    double norm(V<double> &a) {
-        double norm = 0;
-        for (int i = 0; i < a.size(); i ++) {
-            norm += a[i] * a[i];
-        }
-        return sqrt(norm);
-    }
-
-    double cosine_dist(V<double> &a, V<double> &b) {
-        double dist = 0;
-        for (int i = 0; i < a.size(); i ++) {
-            dist += (a[i] * b[i]);
-        }
-        return dist / (norm(a) * norm(b));
-    }
+double norm(V<double> &a) {
+  double norm = 0;
+  for (int i = 0; i < a.size(); i++) {
+    norm += a[i] * a[i];
+  }
+  return sqrt(norm);
 }
 
-namespace hnsw {
-    struct comparePairMax {
-        bool operator() (const P<int, double> &a, const P<int, double> &b) {
-            if (a.second  < b.second) {
-                return true;
-            } else if (a.second == b.second) {
-                return b.first < a.first;
-            } else {
-                return false;
-            }
-        }
-    };
-    
-    V<P<int, double>> SearchLayer(V<double> &user, V<P<int, double>> &candidates, V<int> &indptr, V<int> &index, V<int> &level_offset, int level, V<bool> &visited, V<V<double>> vect, int K) {
-        V<P<int, double>> topk = candidates;
-        while (candidates.size() > 0) {
-            std::pop_heap(candidates.begin(), candidates.end(), comparePairMax());
-            int ep = candidates[candidates.size() - 1].first;
-            candidates.pop_back();
-            int start = indptr[ep] + level_offset[level], end = indptr[ep] + level_offset[level + 1];
-            for (int i = start; i < end; i ++) {
-                if (visited[index[i]] || index[i] == -1) {
-                    continue;
-                }
-                visited[index[i]] = true;
-                double _dist = metric::cosine_dist(user, vect[index[i]]);
-                if (_dist > topk[0].first && topk.size() == K) {
-                    continue;
-                }
-                topk.push_back(P<int, double>(index[i], _dist));
-                std::push_heap(topk.begin(), topk.end(), comparePairMax());
-                if (topk.size() > K) {
-                    std::pop_heap(topk.begin(), topk.end(), comparePairMax());
-                    topk.pop_back();
-                }
-                candidates.push_back(P<int, double>(index[i], _dist));
-                std::push_heap(candidates.begin(), candidates.end(), comparePairMax());
-            }
-        }
-        return topk;
-    }
+double cosine_dist(V<double> &a, V<double> &b) {
+  double dist = 0;
+  for (int i = 0; i < a.size(); i++) {
+    dist += (a[i] * b[i]);
+  }
+  return dist / (norm(a) * norm(b));
+}
+}  // namespace metric
 
-    V<P<int, double>> QueryHNSW(V<double> &user, int ep, V<int> &indptr, V<int> &index, V<int> &level_offset, int max_level, V<V<double>> vect, int K) {
-        V<P<int, double>> topk;
-        topk.push_back(P<int, double>(ep, metric::cosine_dist(user, vect[ep])));
-        V<bool> visited(vect.size(), false);
-        visited[ep] = true;
-        for (int level = max_level; level >= 0; level --) {
-            topk = SearchLayer(user, topk, indptr, index, level_offset, level, visited, vect, K);
-        }
-        return topk;
+namespace hnsw {
+struct comparePairMax {
+  bool operator()(const P<int, double> &a, const P<int, double> &b) {
+    if (a.second < b.second) {
+      return true;
+    } else if (a.second == b.second) {
+      return b.first < a.first;
+    } else {
+      return false;
     }
+  }
+};
+
+V<P<int, double>> SearchLayer(V<double> &user, V<P<int, double>> &candidates,
+                              V<int> &indptr, V<int> &index,
+                              V<int> &level_offset, int level, V<bool> &visited,
+                              V<V<double>> vect, int K) {
+  V<P<int, double>> topk = candidates;
+  while (candidates.size() > 0) {
+    std::pop_heap(candidates.begin(), candidates.end(), comparePairMax());
+    int ep = candidates[candidates.size() - 1].first;
+    candidates.pop_back();
+    int start = indptr[ep] + level_offset[level],
+        end = indptr[ep] + level_offset[level + 1];
+    for (int i = start; i < end; i++) {
+      if (visited[index[i]] || index[i] == -1) {
+        continue;
+      }
+      visited[index[i]] = true;
+      double _dist = metric::cosine_dist(user, vect[index[i]]);
+      if (_dist > topk[0].first && topk.size() == K) {
+        continue;
+      }
+      topk.push_back(P<int, double>(index[i], _dist));
+      std::push_heap(topk.begin(), topk.end(), comparePairMax());
+      if (topk.size() > K) {
+        std::pop_heap(topk.begin(), topk.end(), comparePairMax());
+        topk.pop_back();
+      }
+      candidates.push_back(P<int, double>(index[i], _dist));
+      std::push_heap(candidates.begin(), candidates.end(), comparePairMax());
+    }
+  }
+  return topk;
+}
+
+V<P<int, double>> QueryHNSW(V<double> &user, int ep, V<int> &indptr,
+                            V<int> &index, V<int> &level_offset, int max_level,
+                            V<V<double>> vect, int K) {
+  V<P<int, double>> topk;
+  topk.push_back(P<int, double>(ep, metric::cosine_dist(user, vect[ep])));
+  V<bool> visited(vect.size(), false);
+  visited[ep] = true;
+  for (int level = max_level; level >= 0; level--) {
+    topk = SearchLayer(user, topk, indptr, index, level_offset, level, visited,
+                       vect, K);
+  }
+  return topk;
+}
+}  // namespace hnsw
+
+void read_embeddings(std::string file_name, V<V<double>> &vect) {
+  std::ifstream fin(file_name, std::ios::in | std::ios::binary);
+  unsigned int num_lines, embedding_size = 0;
+  embedding_size = 768;
+  fin.read((char *)&num_lines, sizeof(num_lines));
+  fin.read((char *)&embedding_size, sizeof(embedding_size));
+  printf("Starting to read %d lines, with embedding size: %d\n", num_lines,
+         embedding_size);
+
+  int c = 0;
+  vect.push_back(V<double>());
+  while (fin.good()) {
+    if (c == embedding_size) {
+      vect.push_back(V<double>());
+      c = 0;
+    }
+    double num;
+    fin.read((char *)&num, sizeof(double));
+    vect.back().push_back(num);
+    c++;
+  }
+  fin.close();
+}
+
+void read_vect(std::string file_name, V<int> &vect) {
+  std::ifstream fin(file_name, std::ios::in);
+  std::string line;
+  while (std::getline(fin, line)) {
+    std::stringstream stream(line);
+    int temp;
+    while (stream >> temp) {
+      vect.push_back(temp);
+    }
+  }
+  fin.close();
+  printf("Read vector from %s\n", file_name.c_str());
+  // for (auto &a : vect) {
+  //   std::cout << a << " ";
+  // }
+  // std::cout << std::endl;
+}
+
+void read_int(std::string file_name, int &res) {
+  std::ifstream fin(file_name, std::ios::in);
+  fin >> res;
+  fin.close();
+  printf("Read int from %s : %d\n", file_name.c_str(), res);
 }
 
 int main(int argc, char **argv) {
-    std::string out_dir = argv[1];
-    int K = std::stoi(argv[2]);
-    std::string in_file = argv[3];
-    std::string out_file = argv[4];
-    int max_level, ep;
-    //read max_level
-    std::ifstream fin(out_dir + "/max_level.txt", std::ios::in);
-    fin >> max_level;
-    fin.close();
-    //read ep
-    fin.open(out_dir + "/ep.txt", std::ios::in);
-    fin >> ep;
-    fin.close();
-    V<int> level, index, indptr, level_offset;
-    //read level
-    fin.open(out_dir + "level.txt", std::ios::in);
-    std::string line;
-    while (std::getline(fin, line)) {
-        std::stringstream stream(line);
-        int temp;
-        while (stream >> temp) {
-            level.push_back(temp);
-        }
-    }
-    fin.close();
-    //read index
-    fin.open(out_dir + "/index.txt", std::ios::in);
-    while (std::getline(fin, line)) {
-        std::stringstream stream(line);
-        int temp;
-        while (stream >> temp) {
-            index.push_back(temp);
-        }
-    }
-    fin.close();
-    //read indptr
-    fin.open(out_dir + "/indptr.txt", std::ios::in);
-    while (std::getline(fin, line)) {
-        std::stringstream stream(line);
-        int temp;
-        while (stream >> temp) {
-            indptr.push_back(temp);
-        }
-    }
-    //read level_offset
-    fin.open(out_dir + "/level_offset.txt", std::ios::in);
-    while (std::getline(fin, line)) {
-        std::stringstream stream(line);
-        int temp;
-        while (stream >> temp) {
-            level_offset.push_back(temp);
-        }
-    }
-    fin.close();
-    //read vect
-    V<V<double>> vect;
-    fin.open(out_dir + "/vect.txt", std::ios::in);
-    while (std::getline(fin, line)) {
-        vect.push_back(V<double>());
-        std::stringstream stream(line);
-        double temp;
-        while (stream >> temp) {
-            vect[vect.size() - 1].push_back(temp);
-        }
-    }
-    fin.close();
-    //initialize MPI pipeline
-    int size, rank;
-    MPI_Init(NULL, NULL);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    //read users.bin and divide users
-    MPI_Finalize();
+  std::string out_dir = argv[1];
+  int K = std::stoi(argv[2]);
+  std::string in_file = argv[3];
+  std::string out_file = argv[4];
+  int max_level, ep;
+  V<int> level, index, indptr, level_offset;
+  V<V<double>> user_embeddings, node_embeddings;
+  read_int(out_dir + "/max_level.txt", max_level);
+  read_int(out_dir + "/ep.txt", max_level);
+  read_vect(out_dir + "/level.txt", level);
+  read_vect(out_dir + "/index.txt", index);
+  read_vect(out_dir + "/indptr.txt", indptr);
+  read_vect(out_dir + "/level_offset.txt", level_offset);
+  read_embeddings("out_data/user.bin", user_embeddings);
+  // read_embeddings("out_data/vect.bin", user_embeddings);
+
+  // initialize MPI pipeline
+  int size, rank;
+  MPI_Init(NULL, NULL);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  // read users.bin and divide users
+  MPI_Finalize();
 }
--- Assignment-3/setup.cpp
+++ Assignment-3/setup.cpp
@@ -1,19 +1,43 @@
+#include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <string>
 
-int main(int argc, char **argv) {
-    std::string in_dir = argv[1], out_dir = argv[2];
-    std::ifstream fin(in_dir + "/users.txt", std::ios::in);
-    std::ofstream fout(out_dir + "/users.bin", std::ios::out | std::ios::binary);
-    std::string line;
-    while (std::getline(fin, line)) {
-        std::stringstream stream(line);
-        double temp;
-        while (stream >> temp) {
-            fout.write((char*) &temp, sizeof(double));
-        }
+void convert_to_binary(std::string in_dir, std::string out_dir,
+                       std::string file_name) {
+  std::ifstream fin(in_dir + "/" + file_name + ".txt", std::ios::in);
+  std::ofstream fout(out_dir + "/" + file_name + ".bin",
+                     std::ios::out | std::ios::binary | std::ios::ate);
+  if (!fout) {
+    std::cerr << "Cannot open file" << std::endl;
+    return;
+  }
+  std::string line;
+  unsigned int line_num = 0, embedding_size = 0;
+  fout.seekp(2 * sizeof(line_num), std::ios::beg);
+  while (std::getline(fin, line)) {
+    std::stringstream stream(line);
+    double temp;
+    embedding_size = 0;
+    while (stream >> temp) {
+      fout.write((char *)&temp, sizeof(double));
+      embedding_size++;
     }
-    fin.close();
-    fout.close();
+    line_num++;
+  }
+
+  printf("Read %d lines, with embedding_size: %d\n", line_num, embedding_size);
+  fin.close();
+  fout.seekp(0, std::ios::beg);
+  fout.write((char *)&line_num, sizeof(line_num));
+  fout.write((char *)&embedding_size, sizeof(embedding_size));
+  fout.close();
+}
+
+int main(int argc, char **argv) {
+  std::string in_dir = argv[1], out_dir = argv[2];
+  // std::filesystem::create_directories(out_dir);
+  convert_to_binary(in_dir, out_dir, "user");
+  // convert_to_binary(in_dir, out_dir, "vect");
 }
