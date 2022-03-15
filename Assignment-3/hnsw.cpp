8f473592fffd48f2d5a05467fee7652ecc74f189
Harsh Agrawal <harsh.ag14901@gmail.com>
2022-03-15 18:14:59 +0530

    Bug fixes

--- Assignment-3/hnsw.cpp
+++ Assignment-3/hnsw.cpp
@@ -16,7 +16,8 @@ double cosine_dist(double *a, double *b, int sz) {
   return dist / (norm(a, sz) * norm(b, sz));
 }
 
-// double *get_vec(double *vect, int node, int start, int end, int num_lines, int size, int embedding_size, MPI_Datatype &vector_t, MPI_Win &win) {
+// double *get_vec(double *vect, int node, int start, int end, int num_lines,
+// int size, int embedding_size, MPI_Datatype &vector_t, MPI_Win &win) {
 //   if (node >= start && node < end) {
 //     return &vect[(node - start) * embedding_size];
 //   } else {
@@ -30,7 +31,8 @@ double cosine_dist(double *a, double *b, int sz) {
 //     printf("Fetching from target: %d\n", target);
 //     printf("Fetching from index: %d\n", node - target * num_lines / size);
 //     MPI_Win_fence(0, win);
-//     MPI_Get((void *)v, 1, vector_t, target, node - target * num_lines / size, 1,
+//     MPI_Get((void *)v, 1, vector_t, target, node - target * num_lines / size,
+//     1,
 //             vector_t, win);
 //     MPI_Win_fence(0, win);
 //     printf("Fetching complete!\n");
@@ -40,10 +42,11 @@ double cosine_dist(double *a, double *b, int sz) {
 
 // V<P<int, double>> SearchLayer(double *user, V<P<int, double>> &candidates,
 //                               V<int> &indptr, V<int> &index,
-//                               V<int> &level_offset, int level, V<bool> &visited,
-//                               double *vect, int K, int start_news, int end_news,
-//                               int num_lines, int size, int embedding_size,
-//                               MPI_Datatype &vector_t, MPI_Win &win) {
+//                               V<int> &level_offset, int level, V<bool>
+//                               &visited, double *vect, int K, int start_news,
+//                               int end_news, int num_lines, int size, int
+//                               embedding_size, MPI_Datatype &vector_t, MPI_Win
+//                               &win) {
 //   V<P<int, double>> topk = candidates;
 //   while (candidates.size() > 0) {
 //     std::pop_heap(candidates.begin(), candidates.end(), comparePairMax());
@@ -56,7 +59,8 @@ double cosine_dist(double *a, double *b, int sz) {
 //         continue;
 //       }
 //       visited[index[i]] = true;
-//       double *ep_vec = get_vec(vect, index[i], start_news, end_news, num_lines,
+//       double *ep_vec = get_vec(vect, index[i], start_news, end_news,
+//       num_lines,
 //                                size, embedding_size, vector_t, win);
 //       printf("Fetched embedding for %d\n", index[i]);
 //       double _dist = cosine_dist(user, ep_vec, embedding_size);
@@ -75,20 +79,33 @@ double cosine_dist(double *a, double *b, int sz) {
 //   }
 //   return topk;
 // }
-V<P<int, double>> SearchLayer(double *user, V<P<int, double>> &candidates, V<int> &indptr, V<int> &index, V<int> &level_offset, int level, V<bool> &visited, double *vect, int K, int num_lines, int embedding_size) {
+V<P<int, double>> SearchLayer(double *user, V<P<int, double>> &candidates,
+                              V<int> &indptr, V<int> &index,
+                              V<int> &level_offset, int level, V<bool> &visited,
+                              double *vect, int K, int num_lines,
+                              int embedding_size) {
   V<P<int, double>> topk = candidates;
   while (candidates.size() > 0) {
     std::pop_heap(candidates.begin(), candidates.end(), comparePairMax());
     int ep = candidates[candidates.size() - 1].first;
     candidates.pop_back();
-    int start = indptr[ep] + level_offset[level], end = indptr[ep] + level_offset[level + 1];
+    int start = indptr[ep] + level_offset[level],
+        end = indptr[ep] + level_offset[level + 1];
     for (int i = start; i < end; i++) {
-      if (visited[index[i]] || index[i] == -1) {
+      // printf("Processing embedding for index[%d] = %d\n", i, index[i]);
+      if (i >= 262375800) {
+        printf("start: %d, end: %d, i: %d, ep: %d, level: %d\n", start, end, i,
+               ep, level);
+        assert(false);
+      }
+
+      assert(index[i] < 1305265);
+      if (index[i] == -1 || visited[index[i]]) {
         continue;
       }
       visited[index[i]] = true;
-      printf("Fetched embedding for %d\n", index[i]);
-      double _dist = cosine_dist(user, &vect[index[i] * embedding_size], embedding_size);
+      double _dist =
+          cosine_dist(user, &vect[index[i] * embedding_size], embedding_size);
       if (_dist > topk[0].first && topk.size() == K) {
         continue;
       }
@@ -105,28 +122,42 @@ V<P<int, double>> SearchLayer(double *user, V<P<int, double>> &candidates, V<int
   return topk;
 }
 
-// V<P<int, double>> QueryHNSW(double *user, int ep, V<int> &indptr, V<int> &index, V<int> &level_offset, int max_level, double *vect, int K, int start, int end, int num_lines, int size, int embedding_size, MPI_Datatype &vector_t, MPI_Win &win) {
+// V<P<int, double>> QueryHNSW(double *user, int ep, V<int> &indptr, V<int>
+// &index, V<int> &level_offset, int max_level, double *vect, int K, int start,
+// int end, int num_lines, int size, int embedding_size, MPI_Datatype &vector_t,
+// MPI_Win &win) {
 //   V<P<int, double>> topk;
-//   auto ep_vec = get_vec(vect, ep, start, end, num_lines, size, embedding_size, vector_t, win);
-//   topk.push_back(P<int, double>(ep, cosine_dist(user, ep_vec, embedding_size)));
-//   V<bool> visited(num_lines, false);
-//   visited[ep] = true;
-//   for (int level = max_level; level >= 0; level--) {
+//   auto ep_vec = get_vec(vect, ep, start, end, num_lines, size,
+//   embedding_size, vector_t, win); topk.push_back(P<int, double>(ep,
+//   cosine_dist(user, ep_vec, embedding_size))); V<bool> visited(num_lines,
+//   false); visited[ep] = true; for (int level = max_level; level >= 0;
+//   level--) {
 //     // printf("Calling search layer: %d\n", level);
-//     topk = SearchLayer(user, topk, indptr, index, level_offset, level, visited,
+//     topk = SearchLayer(user, topk, indptr, index, level_offset, level,
+//     visited,
 //                        vect, K, start, end, num_lines, size, embedding_size,
 //                        vector_t, win);
 //   }
 //   return topk;
 // }
-V<P<int, double>> QueryHNSW(double *user, int ep, V<int> &indptr, V<int> &index, V<int> &level_offset, int max_level, double *vect, int K, int num_lines, int embedding_size) {
+V<P<int, double>> QueryHNSW(double *user, int ep, V<int> &indptr, V<int> &index,
+                            V<int> &level_offset, int max_level, double *vect,
+                            int K, int num_lines, int embedding_size) {
   V<P<int, double>> topk;
-  topk.push_back(P<int, double>(ep, cosine_dist(user, &vect[ep * embedding_size], embedding_size)));
+  topk.push_back(P<int, double>(
+      ep, cosine_dist(user, &vect[ep * embedding_size], embedding_size)));
   V<bool> visited(num_lines, false);
   visited[ep] = true;
+  // std::cout << "max_level: " << max_level << std::endl;
+  int thread_id = omp_get_thread_num();
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
   for (int level = max_level; level >= 0; level--) {
+    printf("[%d@%d] Calling search layer %d\n", thread_id, rank, level);
     // printf("Calling search layer: %d\n", level);
-    topk = SearchLayer(user, topk, indptr, index, level_offset, level, visited, vect, K, num_lines, embedding_size);
+    topk = SearchLayer(user, topk, indptr, index, level_offset, level, visited,
+                       vect, K, num_lines, embedding_size);
   }
   return topk;
 }
--- Assignment-3/main.cpp
+++ Assignment-3/main.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 #include <mpi.h>
 #include <omp.h>
+
 #include <chrono>
+
 #include "hnsw.hpp"
 #include "util.hpp"
 
@@ -17,86 +19,107 @@ int main(int argc, char **argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_File fout;
-  //open file to write data to
-  if (MPI_File_open(MPI_COMM_WORLD, out_file.c_str(), MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fout) != MPI_SUCCESS) {
+  // open file to write data to
+  if (MPI_File_open(MPI_COMM_WORLD, out_file.c_str(),
+                    MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL,
+                    &fout) != MPI_SUCCESS) {
     printf("Unable to open file: %s", out_file.c_str());
     return -1;
   }
   MPI_File_close(&fout);
   MPI_Barrier(MPI_COMM_WORLD);
-  //read data
+  // read data
   read_int(out_dir + "/max_level.txt", max_level);
-  read_int(out_dir + "/ep.txt", max_level);
+  read_int(out_dir + "/ep.txt", ep);
   read_vect(out_dir + "/level.bin", level);
   read_vect(out_dir + "/index.bin", index);
   read_vect(out_dir + "/indptr.bin", indptr);
   read_vect(out_dir + "/level_offset.bin", level_offset);
-  //read embeddings
+  // read embeddings
   uint embedding_size, num_users, num_news;
   get_embedding_info(out_dir + "/vect.bin", embedding_size, num_news);
   get_embedding_info(out_dir + "/user.bin", embedding_size, num_users);
-  //create datatype
+  // create datatype
   MPI_Datatype vector_t;
   MPI_Type_contiguous(embedding_size, MPI_DOUBLE, &vector_t);
   MPI_Type_commit(&vector_t);
   double *vect_buff, *user_buff;
   auto begin = std::chrono::high_resolution_clock::now();
-  vect_buff = read_embeddings(out_dir + "/vect.bin", rank, size, vector_t, num_news, embedding_size);
+  vect_buff = read_embeddings(out_dir + "/vect.bin", rank, size, vector_t,
+                              num_news, embedding_size);
   auto end = std::chrono::high_resolution_clock::now();
-  auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+  auto elapsed =
+      std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
   printf("Read vect embedding time: %ld\n", elapsed.count());
   begin = std::chrono::high_resolution_clock::now();
-  user_buff = read_embeddings(out_dir + "/user.bin", rank, size, vector_t, num_users, embedding_size);
+  user_buff = read_embeddings(out_dir + "/user.bin", rank, size, vector_t,
+                              num_users, embedding_size);
   end = std::chrono::high_resolution_clock::now();
   elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
   printf("Read user embedding time: %ld\n", elapsed.count());
-  uint start_news = rank * num_news / size, end_news = (rank + 1) * num_news / size, start_user = rank * num_users / size, end_user = (rank + 1) * num_users / size;
+  uint start_news = rank * num_news / size,
+       end_news = (rank + 1) * num_news / size,
+       start_user = rank * num_users / size,
+       end_user = (rank + 1) * num_users / size;
   V<int> counts, displacements;
   int sum = 0;
-  for (int i = 0; i < size; i ++) {
+  for (int i = 0; i < size; i++) {
     int start_item = i * num_news / size, end_item = (i + 1) * num_news / size;
     counts.push_back(end_item - start_item);
     displacements.push_back(sum);
     sum += (end_item - start_item);
   }
-  double* vect = new double[num_news * embedding_size];
-  MPI_Allgatherv((void*) vect_buff, end_news - start_news, vector_t, vect, counts.data(), displacements.data(), vector_t, MPI_COMM_WORLD);
-  //divide work among threads
+  double *vect = new double[num_news * embedding_size];
+  MPI_Allgatherv((void *)vect_buff, end_news - start_news, vector_t, vect,
+                 counts.data(), displacements.data(), vector_t, MPI_COMM_WORLD);
+  // divide work among threads
   MPI_Datatype items_t;
   MPI_Type_contiguous(K, MPI_UNSIGNED, &items_t);
   MPI_Type_commit(&items_t);
-  MPI_File_open(MPI_COMM_WORLD, out_file.c_str(), MPI_MODE_WRONLY, MPI_INFO_NULL, &fout);
+  MPI_File_open(MPI_COMM_WORLD, out_file.c_str(), MPI_MODE_WRONLY,
+                MPI_INFO_NULL, &fout);
   MPI_File_seek(fout, start_user * K * sizeof(int), MPI_SEEK_SET);
-  V<int> items(K, -1);
-  #pragma omp parallel for ordered
-  for (int i = start_user; i < end_user; i ++) {
-    V<P<int, double>> topk = QueryHNSW(&user_buff[(i - start_user) * ((ll) embedding_size)], ep, indptr, index, level_offset, max_level, vect, K, num_news, embedding_size);
-    for (int j = K; j > 0; j --) {
-      std::pop_heap(topk.begin(), topk.begin() + j, comparePairMax());
-      items[K - j] = topk[j - 1].first;
+  V<V<int>> items(end_user - start_user, V<int>(K, -1));
+#pragma omp parallel
+  {
+    int thread_id = omp_get_thread_num();
+    printf("I am thread: %d @ rank: %d\n", thread_id, rank);
+#pragma omp for
+    for (int i = start_user; i < end_user; i++) {
+      V<P<int, double>> topk = QueryHNSW(
+          &user_buff[(i - start_user) * ((ll)embedding_size)], ep, indptr,
+          index, level_offset, max_level, vect, K, num_news, embedding_size);
+      for (int j = K; j > 0; j--) {
+        std::pop_heap(topk.begin(), topk.begin() + j, comparePairMax());
+        items[i - start_user][K - j] = topk[j - 1].first;
+      }
     }
-    #pragma omp ordered
-    MPI_File_write(fout, (void*) items.data(), 1, items_t, MPI_STATUS_IGNORE);
+  }
+  for (int i = 0; i < end_user - start_user; i++) {
+    MPI_File_write(fout, (void *)items[i].data(), 1, items_t,
+                   MPI_STATUS_IGNORE);
   }
   MPI_File_close(&fout);
-//   MPI_Win window;
-//   std::cout << "Rank and Size: " << rank << " " << size << std::endl;
-  
-//   MPI_Win_create((void *)vect_buff, embedding_size * (end_news - start_news),
-//                  embedding_size * sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD,
-//                  &window);
-//   MPI_Win_fence(0, window);
-// #pragma omp parallel for
-//   {
-//     for (int i = start_user; i < end_user; i++) {
-//       // use double buffer in place of vectors (user, vect)
-//       V<P<int, double>> topk =
-//           QueryHNSW(user_buff, ep, indptr, index, level_offset, max_level,
-//                     vect_buff, K, start_news, end_news, num_news, size,
-//                     embedding_size, vector_t, window);
-//     }
-//   }
-//   MPI_Win_free(&window);
+  //   MPI_Win window;
+  //   std::cout << "Rank and Size: " << rank << " " << size << std::endl;
+
+  //   MPI_Win_create((void *)vect_buff, embedding_size * (end_news -
+  //   start_news),
+  //                  embedding_size * sizeof(double), MPI_INFO_NULL,
+  //                  MPI_COMM_WORLD, &window);
+  //   MPI_Win_fence(0, window);
+  // #pragma omp parallel for
+  //   {
+  //     for (int i = start_user; i < end_user; i++) {
+  //       // use double buffer in place of vectors (user, vect)
+  //       V<P<int, double>> topk =
+  //           QueryHNSW(user_buff, ep, indptr, index, level_offset,
+  //           max_level,
+  //                     vect_buff, K, start_news, end_news, num_news, size,
+  //                     embedding_size, vector_t, window);
+  //     }
+  //   }
+  //   MPI_Win_free(&window);
   MPI_Finalize();
   delete vect;
   delete vect_buff;
