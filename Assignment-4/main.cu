65f3b1e248de7765a416c7eafdf19dc8cf9abb25
ArnavT005 <arnavtuli00@gmail.com>
2022-04-15 04:10:54 +0530

    Final code

--- Assignment-4/main.cu
+++ Assignment-4/main.cu
@@ -7,12 +7,6 @@
 
 __global__
 void find(float th2, int* query_cu, int query_rows, int query_cols, float graysum_avg, int* data_cu, int data_rows, int data_cols, ll* prefixsum_cu, float* result_cu, float* graydiff_cu, float* query_rotated_1_cu, float* query_rotated_2_cu) {
-    // extern __shared__ float rmsd[];
-    // extern __shared__ int arr[];
-    // int row, col, rot, angle;
-    // row = blockIdx.x;
-    // col = blockIdx.y; 
-    // rot = blockIdx.z;
     int bid = blockIdx.x;
     int rot = blockIdx.y;
     int block_sz = 1, angle, row, col;
@@ -23,47 +17,9 @@ void find(float th2, int* query_cu, int query_rows, int query_cols, float graysu
             block_sz = (data_rows * data_cols) / gridDim.x + 1;
         }
     }
-    // for (int i = threadIdx.x; i < block_sz; i += blockDim.x) {
-    //     arr[i] = -1;
-    // } 
     auto start_block = bid * block_sz, end_block = min(start_block + block_sz, data_rows * data_cols);
-    // if (threadIdx.x == 0) {
-    //     int index = 0;
-    //     for (int i = start_block; i < end_block; i ++) {
-    //         row = i / data_cols;
-    //         col = i % data_cols;
-    //         if (rot == 0) {
-    //             angle = 0;
-    //         } else if (rot == 1) {
-    //             angle = 45;
-    //         } else {
-    //             angle = -45;
-    //         }
-    //         auto bb = BB{col, row, query_cols - 1, query_rows - 1};
-    //         if (!bb.rotate(angle, data_rows, data_cols)) {
-    //             continue;
-    //         }
-    //         auto temp = get_prefix_sum(bb, data_rows, data_cols, prefixsum_cu);
-    //         float ps = temp / ((float) 3 * (bb.h + 1) * (bb.w + 1));
-    //         graydiff_cu[row * data_cols * 3 + col * 3 + rot] = abs(ps - graysum_avg);
-    //         if (abs(ps - graysum_avg) >= th2){
-    //             continue;
-    //         }
-    //         arr[index ++] = i;
-    //     }
-    // }
-    // __syncthreads();
-    float data_px[3], query_px[3];
+    float data_px[3]{0, 0, 0}, query_px[3]{0, 0, 0};
     for (int block_idx = start_block + threadIdx.x; block_idx < end_block; block_idx += blockDim.x) {
-        // int row = arr[block_idx] / data_cols, col = arr[block_idx] % data_cols, angle;
-        // // for (int rot = 0; rot < 3; rot ++) {
-        // if (rot == 0) {
-        //     angle = 0;
-        // } else if (rot == 1) {
-        //     angle = 45;
-        // } else {
-        //     angle = -45;
-        // }
         row = block_idx / data_cols;
         col = block_idx % data_cols;
         if (rot == 0) {
@@ -88,17 +44,14 @@ void find(float th2, int* query_cu, int query_rows, int query_cols, float graysu
         for(auto query_idx = start; query_idx < end; query_idx++){
             int curr_col = query_idx % query_cols;
             int curr_row = query_idx / query_cols;
-            // auto rotated_point = rotate_point(Point{(float) col, (float) row}, Point{(float) curr_col, (float) curr_row}, angle, query_rows);
             Point rotated_point;
             if (rot == 0) {
                 rotated_point.x = col + curr_col;
                 rotated_point.y = row - (query_rows - 1) + curr_row;
             } else if (rot == 1) {
-                // rotated_point = rotate_point(Point{(float) col, (float) row}, Point{(float) curr_col, (float) curr_row}, angle, query_rows);
                 rotated_point.x = col + query_rotated_1_cu[curr_row * query_cols * 2 + curr_col * 2];
                 rotated_point.y = row + query_rotated_1_cu[curr_row * query_cols * 2 + curr_col * 2 + 1];
             } else {
-                // rotated_point = rotate_point(Point{(float) col, (float) row}, Point{(float) curr_col, (float) curr_row}, angle, query_rows);
                 rotated_point.x = col + query_rotated_2_cu[curr_row * query_cols * 2 + curr_col * 2];
                 rotated_point.y = row + query_rotated_2_cu[curr_row * query_cols * 2 + curr_col * 2 + 1];
             }
@@ -114,170 +67,7 @@ void find(float th2, int* query_cu, int query_rows, int query_cols, float graysu
             rmsd += ((data_px[0] - query_px[0]) * (data_px[0] - query_px[0])) + ((data_px[1] - query_px[1]) * (data_px[1] - query_px[1])) + ((data_px[2] - query_px[2]) * (data_px[2] - query_px[2]));
         }
         result_cu[row * data_cols * 3 + col * 3 + rot] = sqrt(rmsd / (query_rows * query_cols * 3));
-        if (row == 1199 && col == 1000 && rot == 2) {
-            printf("RMSD: %f\n", result_cu[row * data_cols * 3 + col * 3 + rot]);
-        }
-        // }
     }
-
-    // int warp_id = threadIdx.x / 32;
-    // const int warp_count = 16;
-    // for (auto block_idx = start_block + warp_id; block_idx < end_block; block_idx += warp_count) {
-    //     // __syncthreads();
-    //     int col = block_idx % data_cols;
-    //     int row = block_idx / data_cols;
-    //     int angle;
-    //     // if (row > 400) {
-    //     //     continue;
-    //     // }
-        
-    //     for (int rot = 0; rot < 3; rot ++) {
-    //         // __syncthreads();
-    //         if (rot == 0) {
-    //             angle = 0;
-    //         } else if (rot == 1) {
-    //             angle = 45;
-    //         } else {
-    //             angle = -45;
-    //         }
-    //         // if (threadIdx.x == 0 && row == 199 && col == 201 && rot == 2)
-    //         // printf("Row: %d, Col: %d", row, col);
-    //         auto bb = BB{col, row, query_cols - 1, query_rows - 1};
-    //         if (!bb.rotate(angle, data_rows, data_cols)) {
-    //             continue;
-    //         }
-    //         auto ps = get_prefix_sum(bb, data_rows, data_cols, prefixsum_cu);
-    //         ps /= ((bb.h + 1) * (bb.w + 1));
-    //         if (abs(ps - graysum_avg) >= th2){
-    //             continue;
-    //         }
-    //         int tid = threadIdx.x;
-    //         int chunk_sz = 1;
-    //         if (query_rows * query_cols > 32) {
-    //             if ((query_rows * query_cols) % 32 == 0) {
-    //                 chunk_sz = (query_rows * query_cols) / 32;
-    //             } else {
-    //                 chunk_sz = (query_rows * query_cols) / 32 + 1;
-    //             }
-    //         }
-    //         int start = (tid - warp_id * 32) * chunk_sz, end = min(start + chunk_sz, query_rows * query_cols);
-    //         rmsd[tid] = 0.0f;
-    //         for(auto query_idx = start; query_idx < end; query_idx++){
-    //             int curr_col = query_idx % query_cols;
-    //             int curr_row = query_idx / query_cols;
-    //             for(auto ch = 0; ch < 3; ch++){
-    //                 float data_px, query_px = query_cu[query_idx * 3 + ch];
-    //                 auto rotated_point = rotate_point(Point{(float) col, (float) row}, Point{(float) curr_col, (float) curr_row}, angle, query_rows);
-    //                 if (angle != 0) {
-    //                     data_px = bilinear_interpolate(rotated_point, ch, data_rows, data_cols, data_cu);
-    //                 } else {
-    //                     data_px = get_value(data_cu, rotated_point.y, rotated_point.x, ch, data_rows, data_cols);
-    //                 }
-    //                 rmsd[tid] += ((data_px - query_px) * (data_px - query_px));
-    //             }
-    //         }
-    //         __syncwarp();
-    //         if (tid == warp_id * 32) {
-    //             float rmsd_val = 0.0f;
-    //             for (int i = tid; i < tid + 32; i ++) {
-    //                 rmsd_val += rmsd[i];
-    //             }
-    //             result_cu[row * data_cols * 3 + col * 3 + rot] = sqrt(rmsd_val / (query_rows * query_cols * 3));
-                
-    //         }
-    //         __syncwarp();
-    //     }
-    // }
-    
-    // if (rot == 0) {
-    //     angle = 0;
-    // } else if (rot == 1) {
-    //     angle = 45;
-    // } else {
-    //     angle = -45;
-    // }
-    // auto bb = BB{col, row, query_cols - 1, query_rows - 1};
-    // if (!bb.rotate(angle, gridDim.x, gridDim.y)) {
-    //     return;
-    // }
-    // auto temp = get_prefix_sum(bb, gridDim.x, gridDim.y, prefixsum_cu);
-    // float ps = temp / ((float) 3 * (bb.h + 1) * (bb.w + 1));
-    // if (threadIdx.x == 0) {
-    //     // if (row == 214 && col == 239 && rot == 1) {
-    //     //     printf("BB: %d, %d, %d, %d\n", bb.y, bb.x, bb.w, bb.h);
-    //     //     printf("PS: %d, %f\n", temp, ps);
-    //     //     ll sum = 0;
-    //     //     for (int i = 103; i <= 214; i ++) {
-    //     //         for (int j = 184; j <= 294; j ++) {
-    //     //             for (int k = 0; k < 3; k ++) {
-    //     //                 sum += (ll) data_cu[i * data_cols * 3 + j * 3 + k];
-    //     //             }
-    //     //         }
-    //     //     }
-    //     //     float temp_sum = sum / ((float) 3 * 112 * 111);
-    //     //     printf("VERIF: %d, %f\n", sum, temp_sum);
-    //     //     printf("GRAYSUM: %f\n", graysum_avg);
-    //     //     ll new_sum = 0;
-    //     //     for (int i = 0; i < query_rows; i ++) {
-    //     //         for (int j = 0; j < query_cols; j ++) {
-    //     //             for (int k = 0; k < 3; k ++) {
-    //     //                 new_sum += (ll) query_cu[i * query_cols * 3 + j * 3 + k];
-    //     //             }
-    //     //         }
-    //     //     }
-    //     //     printf("GRAYSUM INT: %d, %f\n", new_sum, new_sum / ((float) query_cols * query_rows * 3));
-    //     // }
-    //     graydiff_cu[row * gridDim.y * gridDim.z + col * gridDim.z + rot] = abs(ps - graysum_avg);
-    // }
-    // if (abs(ps - graysum_avg) >= th2){
-    //     return;
-    // }
-
-    // int tid = threadIdx.x;
-    // int chunk_sz = 1;
-    // if (query_rows * query_cols > blockDim.x) {
-    //     if ((query_rows * query_cols) % blockDim.x == 0) {
-    //         chunk_sz = (query_rows * query_cols) / blockDim.x;
-    //     } else {
-    //         chunk_sz = (query_rows * query_cols) / blockDim.x + 1;
-    //     }
-    // }
-    // int start = tid * chunk_sz, end = min(start + chunk_sz, query_rows * query_cols);
-    // rmsd[tid] = 0.0f;
-    // for(auto query_idx = start; query_idx < end; query_idx++){
-    //     int curr_col = query_idx % query_cols;
-    //     int curr_row = query_idx / query_cols;
-    //     for(auto ch = 0; ch < 3; ch++){
-    //         float data_px, query_px = query_cu[query_idx * 3 + ch];
-    //         auto rotated_point = rotate_point(Point{(float) col, (float) row}, Point{(float) curr_col, (float) curr_row}, angle, query_rows);
-    //         if (angle != 0) {
-    //             data_px = bilinear_interpolate(rotated_point, ch, gridDim.x, gridDim.y, data_cu);
-    //         } else {
-    //             data_px = get_value(data_cu, rotated_point.y, rotated_point.x, ch, gridDim.x, gridDim.y);
-    //         }
-    //         rmsd[tid] += ((data_px - query_px) * (data_px - query_px));
-    //     }
-    // }
-    // __syncthreads();
-    // if (threadIdx.x == 0) {
-    //     float rmsd_val = 0.0f;
-    //     for (int i = 0; i < blockDim.x; i ++) {
-    //         rmsd_val += rmsd[i];
-    //     }
-    //     result_cu[row * gridDim.y * gridDim.z + col * gridDim.z + rot] = sqrt(rmsd_val / (query_rows * query_cols * 3));
-    // }
-    // // int step = 2, thread_count = blockDim.x;
-    // // while (thread_count > 1) {
-    // //     if (tid % step == 0) {
-    // //         rmsd[tid] += rmsd[tid + (step / 2)];
-    // //     }
-    // //     step *= 2;
-    // //     thread_count /= 2;
-    // //     __syncthreads();
-    // // }
-    // // if (tid == 0) {
-    // //     result_cu[row * gridDim.y * gridDim.z + col * gridDim.z + rot] = sqrt(rmsd[tid] / (query_rows * query_cols * 3));
-    // // }
 }
 
 struct comparePairMax {
@@ -312,13 +102,12 @@ int main(int argc, char** argv) {
 
     int data_mem = data_rows * data_cols * data_dim, query_mem = query_rows * query_cols * query_dim;
 
-    // auto gray_data_mat = rgb2gray(data_mat);
     auto prefixsum_mat = prefixsum(data_mat);
     auto graysum_avg = graysum(query_mat);
 
     V<float> result_arr(data_mem, std::numeric_limits<float>::max());
     V<float> graydiff_arr(data_mem, std::numeric_limits<float>::max());
-    V<float> query_rotated_1_arr(query_cols * query_rows * 2, 0), query_rotated_2_arr(query_cols * query_rows * 2);
+    V<float> query_rotated_1_arr(query_cols * query_rows * 2), query_rotated_2_arr(query_cols * query_rows * 2);
 
     rotate_matrix(query_rows, query_cols, 45, query_rotated_1_arr.data());
     rotate_matrix(query_rows, query_cols, -45, query_rotated_2_arr.data());
@@ -328,6 +117,7 @@ int main(int argc, char** argv) {
     ll *prefixsum_cu;
     float *graydiff_cu;
     float *query_rotated_1_cu, *query_rotated_2_cu;
+
     cudaMalloc(&data_cu, data_mem * sizeof(int)); 
     cudaMalloc(&query_cu, query_mem * sizeof(int));
     cudaMalloc(&prefixsum_cu, data_rows * data_cols * sizeof(ll));
@@ -344,29 +134,15 @@ int main(int argc, char** argv) {
     cudaMemcpy(query_rotated_2_cu, query_rotated_2_arr.data(), query_rows * query_cols * 2 * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(graydiff_cu, graydiff_arr.data(), data_rows * data_cols * 3 * sizeof(float), cudaMemcpyHostToDevice);
 
-    // dim3 grid_dim = dim3(data_rows, data_cols, 3);
     dim3 grid_dim = dim3(std::min(65536, (data_rows * data_cols) / 1024), 3);
     dim3 block_dim = dim3(std::min(128, query_rows * query_cols));
-
-    int block_sz = 1;
-    if (data_rows * data_cols > grid_dim.x) {
-        if ((data_rows * data_cols) % grid_dim.x == 0) {
-            block_sz = (data_rows * data_cols) / grid_dim.x;
-        } else {
-            block_sz = (data_rows * data_cols) / grid_dim.x + 1;
-        }
-    }
     
     // invoke kernel
-    // find<<<grid_dim, block_dim, block_dim.x * sizeof(float)>>>(th2, query_cu, query_rows, query_cols, graysum_avg, data_cu, prefixsum_cu, result_cu);
     find<<<grid_dim, block_dim>>>(th2, query_cu, query_rows, query_cols, graysum_avg, data_cu, data_rows, data_cols, prefixsum_cu, result_cu, graydiff_cu, query_rotated_1_cu, query_rotated_2_cu);
+    cudaDeviceSynchronize();
+
     cudaMemcpy(result_arr.data(), result_cu, data_mem * sizeof(float), cudaMemcpyDeviceToHost);
     cudaMemcpy(graydiff_arr.data(), graydiff_cu, data_mem * sizeof(float), cudaMemcpyDeviceToHost);
-    
-    auto err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        std::cout << "CUDA Error: " << cudaGetErrorString(err) << std::endl;
-    }
     
     std::priority_queue<P<int, float>, V<P<int, float>>, comparePairMax> result_que;
     for (int i = 0; i < result_arr.size(); i ++) {
--- Assignment-4/util.cu
+++ Assignment-4/util.cu
@@ -26,7 +26,6 @@ void get_value(int* arr, int i, int j, int k, int rows, int cols, float *px){
   for (int ch = 0; ch < 3; ch ++) {
     px[ch] = arr[i * cols * 3 + j * 3 + ch];
   }
-  // return arr[i * cols * 3 + j * 3 + k];
 }
 
 __device__
@@ -47,10 +46,6 @@ void bilinear_interpolate(Point p, int ch, int rows, int cols, int* data, float 
   int xh = ceil(p.x);
   int yh = ceil(p.y);
   float bl_val[3]{0, 0, 0}, br_val[3]{0, 0, 0}, tl_val[3]{0, 0, 0}, tr_val[3]{0, 0, 0};
-  // auto bl_val = get_value(data, yl, xl, ch, rows, cols);
-  // auto br_val = get_value(data, yl, xh, ch, rows, cols);
-  // auto tl_val = get_value(data, yh, xl, ch, rows, cols);
-  // auto tr_val = get_value(data, yh, xh, ch, rows, cols);
   get_value(data, yl, xl, ch, rows, cols, bl_val);
   get_value(data, yl, xh, ch, rows, cols, br_val);
   get_value(data, yh, xl, ch, rows, cols, tl_val);
@@ -58,7 +53,6 @@ void bilinear_interpolate(Point p, int ch, int rows, int cols, int* data, float 
   float x_frac, y_frac, x_inv_frac, y_inv_frac;
   if(xl == xh) {
     if (yl == yh) {
-      // return bl_val;
       data_px[0] = bl_val[0];
       data_px[1] = bl_val[1];
       data_px[2] = bl_val[2];
@@ -70,7 +64,6 @@ void bilinear_interpolate(Point p, int ch, int rows, int cols, int* data, float 
     data_px[1] = bl_val[1] * y_inv_frac + tl_val[1] * y_frac;
     data_px[2] = bl_val[2] * y_inv_frac + tl_val[2] * y_frac;
     return;
-    // return (bl_val * y_inv_frac + tl_val * y_frac);
   }
   if(yl == yh) {
     x_frac = (p.x - xl)/(xh - xl);
@@ -79,21 +72,16 @@ void bilinear_interpolate(Point p, int ch, int rows, int cols, int* data, float 
     data_px[1] = bl_val[1] * x_inv_frac + br_val[1] * x_frac;
     data_px[2] = bl_val[2] * x_inv_frac + br_val[2] * x_frac;
     return;
-    // return (bl_val * x_inv_frac + br_val * x_frac);
   }
   x_frac = (p.x - xl)/(xh - xl);
   y_frac = (p.y - yl)/(yh - yl);
   
   x_inv_frac = 1 - x_frac;
   y_inv_frac = 1 - y_frac;
-  
   data_px[0] = (bl_val[0] * x_inv_frac + br_val[0] * x_frac) * y_inv_frac + (tl_val[0] * x_inv_frac + tr_val[0] * x_frac) * y_frac;
   data_px[1] = (bl_val[1] * x_inv_frac + br_val[1] * x_frac) * y_inv_frac + (tl_val[1] * x_inv_frac + tr_val[1] * x_frac) * y_frac;
   data_px[2] = (bl_val[2] * x_inv_frac + br_val[2] * x_frac) * y_inv_frac + (tl_val[2] * x_inv_frac + tr_val[2] * x_frac) * y_frac;
-  
   return;
-  // float ret = (bl_val * x_inv_frac + br_val * x_frac) * y_inv_frac + (tl_val * x_inv_frac + tr_val * x_frac) * y_frac;
-  // return ret;
 }
 
 __device__
